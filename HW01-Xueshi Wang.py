017f6db37652a205c2ec605c8d9b95c570d9de5e
Xueshi Wang <44184324+wbwxshao@users.noreply.github.com>
2019-01-27 17:03:29 -0500

    Add files via upload

---
+++ HW01-Xueshi Wang.py
@@ -0,0 +1,8 @@
+"""Xueshi Wang SSW 567 HW01
+   2019.01.27
+"""
+
+def classify_triangle(a, b, c):
+    """The function returns a string that specifies whether the triangle is scalene, isosceles, or equilateral, and whether it is a right triangle as well."""
+    
+   
